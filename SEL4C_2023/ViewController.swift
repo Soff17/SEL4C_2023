bc8775e454b96f828547e2106fe5bf3164ab732e
Sofia Donlucas Bañuelos <71408638+Soff17@users.noreply.github.com>
2023-09-04 17:38:55 -0600

    Initial Commit

---
+++ SEL4C_2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SEL4C_2023
+//
+//  Created by Sofía Donlucas on 04/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
